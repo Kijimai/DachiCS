3268a0b16c5d37d0e81b0d46b35cc4edbb699b58
Kijimai <jdbucog@yahoo.com>
2022-06-12 14:27:00 -0700

    basic route redirects

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,20 +8,41 @@ namespace DojoDachi.Controllers;
 public class HomeController : Controller
 {
     [HttpGet("")]
-    public IActionResult Index() {
+    public IActionResult Index()
+    {
         // if(HttpContext.Session.GetString("MyPet") == null) {
         //     HttpContext.Session.SetString("MyPet", "");
         // }
 
-        Dojodachi myPet =  new Dojodachi() {
+        Dojodachi myPet = new Dojodachi()
+        {
             Happiness = 20,
             Fullness = 20,
             Energy = 50,
             Meals = 3
         };
-
-        
-
         return View("Index", myPet);
+    }
+
+    [HttpPost("/feed")]
+    public IActionResult Feed()
+    {
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost("/play")]
+    public IActionResult Play()
+    {
+        return RedirectToAction("Index");
+    }
+    [HttpPost("/work")]
+    public IActionResult Work()
+    {
+        return RedirectToAction("Index");
+    }
+    [HttpPost("/sleep")]
+    public IActionResult Sleep()
+    {
+        return RedirectToAction("Index");
     }
 }
